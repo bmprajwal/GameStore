e88b5b24290d24f73b63f5b47453c15640839360
BM Prajwal <prajwalbm2003@gmail.com>
2024-11-12 21:37:48 +0530

    added cors

--- GameStoreAPI/Program.cs
+++ GameStoreAPI/Program.cs
@@ -3,10 +3,22 @@ using GameStoreAPI.Endpoints;
 
 var builder = WebApplication.CreateBuilder(args);
 
+builder.Services.AddCors(options =>
+{
+  options.AddPolicy("AllowLocalhost", policy =>
+  {
+    policy.WithOrigins("http://localhost:3000")
+            .AllowAnyMethod() 
+            .AllowAnyHeader(); 
+  });
+});
+
 var connString = builder.Configuration.GetConnectionString("GameStoreConnection");
 builder.Services.AddSqlite<GameStoreContext>(connString);
 
 var app = builder.Build();
+
+app.UseCors("AllowLocalhost");
 
 app.MapGamesEndpoints();
 app.MapGenresEndpoints();
